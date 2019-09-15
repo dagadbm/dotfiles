b929a9d6317143ca507e023e8414284325185bb1
David Maia <david.maia@mash.com>
2019-09-15 13:14:07 +0100

    add brew file and defaults placeholder. update volta check

---
+++ macos/defaults.sh
@@ -0,0 +1,4 @@
+# inspiration
+# http://mths.be/macos
+# https://github.com/kevinSuttle/macOS-Defaults/blob/master/REFERENCE.md
+# https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
