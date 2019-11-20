b142eb4b2e3396393d4b370b757dd617f08e596c
David Maia <david.maia@unbabel.com>
2019-11-20 10:36:08 +0000

    Add tigrc. Fix fzf

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -95,8 +95,8 @@ FZF_COMMON_OPTIONS="
   --bind='?:toggle-preview'
   --bind='ctrl-u:preview-page-up'
   --bind='ctrl-d:preview-page-down'
-  --preview-window 'right:60%:hidden'
-  --preview '[[ -d {} ]] && tree -C {} || [[ -f {} ]] && bat --style=full --color=always {} || echo {}'"
+  --preview-window 'right:60%:hidden:wrap'
+  --preview '([[ -d {} ]] && tree -C {}) || ([[ -f {} ]] && bat --style=full --color=always {}) || echo {}'"
 
 command -v fd > /dev/null && export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 command -v bat > /dev/null && command -v tree > /dev/null && export FZF_DEFAULT_OPTS="$FZF_COMMON_OPTIONS"
