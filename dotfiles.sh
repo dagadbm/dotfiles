efefd0e6fd76ee66e058a6d31e4703ef075d360f
David Maia <david.maia@unbabel.com>
2020-06-11 18:18:23 +0100

    fix removal of symlinks

--- dotfiles.sh
+++ dotfiles.sh
@@ -11,3 +11,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
+
+# symlink all zsh-theme files under custom/themes folder
+# this operation cannot be done using dotbot unfortunately
+for f in $(find $ZSH/custom/themes -name "*.zsh-theme"); do ln -f -s $f $ZSH/custom/themes; done
