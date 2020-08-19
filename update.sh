3b829835eaccc13ba92f98f6f75da7ed65de3642
David Maia <david.maia@unbabel.com>
2020-08-19 13:25:15 +0100

    update submodules. improve update.sh scriptt

--- update.sh
+++ update.sh
@@ -1,12 +1,17 @@
+# submodules
 git submodule foreach --recursive git reset --hard
 git submodule update --recursive --remote --merge
 git submodule foreach --recursive git reset --hard
 
-# after updating the submodules you must re-run dotbo
-./dotbot.sh
+# nvim
+nvim +PlugClean +PlugInstall +PlugUpdate +CocInstall +CocUpdateSync +CocRebuild +qall
 
-## upgrade nvim
-nvim +PlugInstall +PlugUpdate +CocInstall +CocUpdateSync +CocRebuild +qall
+# tmux
+~/.tmux/plugins/tpm/bin/clean_plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
 
-## upgrade brew
+# brew
 brew cleanup && brew update && brew upgrade
+
+
