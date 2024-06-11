cb17cd9c8715e8f0919d06dd77e65c5eae4ea510
David Barbosa Maia <dagadbm@gmail.com>
2024-06-11 12:41:57 +0100

    update. change tmux to top as it hides all logs

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# tmux
+~/.tmux/plugins/tpm/bin/clean_plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
+pushd ~/.tmux/plugins/tmux-thumbs
+./tmux-thumbs-install.sh update
+popd
+
 # submodules
 git submodule foreach --recursive git reset --hard
 git submodule update --init --recursive --remote
@@ -8,14 +16,6 @@ git submodule foreach --recursive git reset --hard
 # brew
 brew bundle --file macos/Brewfile
 brew update && brew upgrade && brew cleanup && brew doctor
-
-# tmux
-~/.tmux/plugins/tpm/bin/clean_plugins
-~/.tmux/plugins/tpm/bin/install_plugins
-~/.tmux/plugins/tpm/bin/update_plugins all
-pushd ~/.tmux/plugins/tmux-thumbs
-./tmux-thumbs-install.sh update
-popd
 
 # fzf
 ~/.fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
